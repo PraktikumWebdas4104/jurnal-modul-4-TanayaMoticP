27ba28fbf6134b616d8675a75d361c05d2b2b7df
Tanaya Motic Permatasari <43507919+TanayaMoticP@users.noreply.github.com>
2018-09-28 10:07:39 +0700

    Proses
    Dalam Proses tersebut menggunakan fungsi checkbox dan array. kemudian terdapat reset(belum berfungsi), kemudian upload foto (belum berfungsi).

--- proses.php
+++ proses.php
@@ -1,4 +1,32 @@
-<?php
+<table> 
+	<tr>
+		<td> Hobi :<td>
+		</td>
+	</td>
+</tr>
+</table>
+
+<table>
+		<td><input type="checkbox" name="Hobi[]" value="Berenang"> Berenang </input> <br>		
+			<input type="checkbox" name="Hobi[]" value="Bernyanyi"> Bernyanyi </input><br>
+			<input type="checkbox" name="Hobi[]" value="Travelling"> Travelling </input><br>
+			<input type="submit" name="kirim">
+			<input type="reset" name="reset" value="reset">
+		</td>
+	</td>
+</td>
+</tr>
+
+</table> 
 
 
-?>
+
+
+<?php  
+//if (isset($_POST['kirim'])) {
+//data = $_POST["Hobi"]
+	//foreach($data as $nilai)
+	
+//} echo $nilai <br> 
+
+//?>
